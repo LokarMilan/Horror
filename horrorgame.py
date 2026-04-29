5db8fae8dac4cf1b614c3259d16c04bedf3719d0
LokarMilan <lokar.milan.albert.2024i@bankitatabanya.hu>
2026-04-29 16:18:58 +0200

    enemy 4

--- horrorgame.py
+++ horrorgame.py
@@ -47,6 +47,7 @@ enemy_x, enemy_y = 3, 15
 enemy1_x, enemy1_y = 9, 15
 enemy2_x, enemy2_y = 3, 12
 enemy3_x, enemy3_y = 9, 12
+enemy4_x, enemy4_y = 3, 8
 
 
 menu_music = pygame.mixer.Sound("./sound/menu.ogg") 
@@ -65,6 +66,8 @@ enemy2_hp = 300
 enemy2_alive = True
 enemy3_hp = 400
 enemy3_alive = True
+enemy4_hp = 500
+enemy4_alive = True
 shoot_flash = 0
 shot = False
 gun_state = "idle"
@@ -79,6 +82,7 @@ enemy_img = pygame.image.load("./img/ghost.png").convert_alpha()
 enemy1_img = pygame.image.load("./img/ZOMBE1.png").convert_alpha()
 enemy2_img = pygame.image.load("./img/enemy2.png").convert_alpha()
 enemy3_img = pygame.image.load("./img/org.png").convert_alpha()
+enemy4_img = pygame.image.load("./img/enemy4.png").convert_alpha()
 door_texture = pygame.image.load("./img/wood_door_01.png").convert()
 gun_idle_img = pygame.image.load("./img/gun.png").convert_alpha()
 gun_shoot_img = pygame.image.load("./img/shoot.png").convert_alpha()
@@ -326,7 +330,7 @@ def draw_enemy():
 
 
 def shoot():
-    global enemy_hp, enemy_alive, shoot_flash, gun_state, gun_timer, gun_shake_x, gun_shake_y,enemy1_alive,enemy1_hp,enemy2_alive,enemy2_hp,enemy3_alive,enemy3_hp
+    global enemy_hp, enemy_alive, shoot_flash, gun_state, gun_timer, gun_shake_x, gun_shake_y,enemy1_alive,enemy1_hp,enemy2_alive,enemy2_hp,enemy3_alive,enemy3_hp,enemy4_alive,enemy4_hp
     shoot_flash = 5
     gun_state = "shoot"
     gun_timer = 5
@@ -443,9 +447,37 @@ def shoot():
                 enemy3_alive = False
                 print("ENEMY DEAD")
 
+    if enemy4_alive:
+        dx = enemy4_x - player_x
+        dy = enemy4_y - player_y
+
+        dist = math.sqrt(dx * dx + dy * dy)
+
+        angle_to_enemy4 = math.atan2(dy, dx)
+        angle_diff = angle_to_enemy4 - player_angle
+
+        angle_diff = (angle_diff + math.pi) % (2 * math.pi) - math.pi
+
+        if abs(angle_diff) < 0.1:
+            ray_x = player_x
+            ray_y = player_y
+
+            for i in range(int(dist * 10)):
+                ray_x += math.cos(player_angle) * 0.1
+                ray_y += math.sin(player_angle) * 0.1
+
+                if world_map[int(ray_y)][int(ray_x)] == 1:
+                    return
+            enemy4_hp -= 25
+            print("HIT!", enemy4_hp)
+
+            if enemy4_hp <= 0:
+                enemy4_alive = False
+                print("ENEMY DEAD")
+
 
 def melee_attack():
-    global enemy_hp, enemy_alive,enemy1_hp,enemy1_alive,enemy2_alive,enemy2_hp,enemy3_alive,enemy3_hp
+    global enemy_hp, enemy_alive,enemy1_hp,enemy1_alive,enemy2_alive,enemy2_hp,enemy3_alive,enemy3_hp,enemy4_alive,enemy4_hp
 
     if enemy_alive:
         dx = enemy_x - player_x
@@ -501,6 +533,21 @@ def melee_attack():
             if enemy3_hp <= 0:
                 enemy3_alive = False
                 print("ENEMY DEAD")
+    
+    if enemy4_alive:
+        dx = enemy4_x - player_x
+        dy = enemy4_y - player_y
+
+        dist = math.sqrt(dx * dx + dy * dy)
+
+        if dist < 1.5:
+            enemy4_hp -= 50
+            print("MELEE HIT", enemy4_hp)
+
+            if enemy4_hp <= 0:
+                enemy4_alive = False
+                print("ENEMY DEAD")
+
 
 
 def draw_enemy1():
@@ -599,6 +646,37 @@ def draw_enemy3():
         (screen_x - size // 2, HEIGHT // 2 - size // 2)
     )
 
+def draw_enemy4():
+    if not enemy4_alive:
+        return
+    dx = enemy4_x - player_x
+    dy = enemy4_y - player_y
+
+    dist = math.sqrt(dx * dx + dy * dy)
+
+    angle = math.atan2(dy, dx) - player_angle
+
+    angle = (angle + math.pi) % (2 * math.pi) - math.pi
+
+    if abs(angle) > math.pi / 4:
+        return
+
+    screen_x = int((angle / (math.pi / 4)) * (WIDTH / 2) + WIDTH / 2)
+
+    if screen_x < 0 or screen_x >= WIDTH:
+        return
+
+    if dist > z_buffer[screen_x]:
+        return
+
+    size = int(min(800 / (dist + 0.1), HEIGHT))
+
+    sprite = pygame.transform.scale(enemy4_img, (size, size))
+
+    screen.blit(
+        sprite,
+        (screen_x - size // 2, HEIGHT // 2 - size // 2)
+    )
 
 # ---------------- GAME LOOP ----------------
 running = True
@@ -744,6 +822,7 @@ while running:
         draw_enemy1()
         draw_enemy2()
         draw_enemy3()
+        draw_enemy4()
         draw_flashlight()
         pygame.draw.line(screen, (255, 255, 255), (WIDTH // 2 - 10, HEIGHT // 2), (WIDTH // 2 + 10, HEIGHT // 2), 2)
         pygame.draw.line(screen, (255, 255, 255), (WIDTH // 2, HEIGHT // 2 - 10), (WIDTH // 2, HEIGHT // 2 + 10), 2)
