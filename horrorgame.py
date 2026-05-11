b5460fe50d2c3c87153ec1158858fc1b2d05926a
LokarMilan <lokar.milan.albert.2024i@bankitatabanya.hu>
2026-05-11 08:34:41 +0200

    enemy5

--- horrorgame.py
+++ horrorgame.py
@@ -48,6 +48,7 @@ enemy1_x, enemy1_y = 9, 15
 enemy2_x, enemy2_y = 3, 12
 enemy3_x, enemy3_y = 9, 12
 enemy4_x, enemy4_y = 3, 8
+enemy5_x, enemy5_y = 9, 8
 
 
 menu_music = pygame.mixer.Sound("./sound/menu.ogg") 
@@ -68,6 +69,8 @@ enemy3_hp = 400
 enemy3_alive = True
 enemy4_hp = 500
 enemy4_alive = True
+enemy5_hp = 600
+enemy5_alive = True
 shoot_flash = 0
 shot = False
 gun_state = "idle"
@@ -83,6 +86,7 @@ enemy1_img = pygame.image.load("./img/ZOMBE1.png").convert_alpha()
 enemy2_img = pygame.image.load("./img/enemy2.png").convert_alpha()
 enemy3_img = pygame.image.load("./img/org.png").convert_alpha()
 enemy4_img = pygame.image.load("./img/enemy4.png").convert_alpha()
+enemy5_img = pygame.image.load("./img/enemy5.png").convert_alpha()
 door_texture = pygame.image.load("./img/wood_door_01.png").convert()
 gun_idle_img = pygame.image.load("./img/gun.png").convert_alpha()
 gun_shoot_img = pygame.image.load("./img/shoot.png").convert_alpha()
@@ -330,7 +334,7 @@ def draw_enemy():
 
 
 def shoot():
-    global enemy_hp, enemy_alive, shoot_flash, gun_state, gun_timer, gun_shake_x, gun_shake_y,enemy1_alive,enemy1_hp,enemy2_alive,enemy2_hp,enemy3_alive,enemy3_hp,enemy4_alive,enemy4_hp
+    global enemy_hp, enemy_alive, shoot_flash, gun_state, gun_timer, gun_shake_x, gun_shake_y,enemy1_alive,enemy1_hp,enemy2_alive,enemy2_hp,enemy3_alive,enemy3_hp,enemy4_alive,enemy4_hp,enemy5_hp, enemy5_alive
     shoot_flash = 5
     gun_state = "shoot"
     gun_timer = 5
@@ -474,10 +478,38 @@ def shoot():
             if enemy4_hp <= 0:
                 enemy4_alive = False
                 print("ENEMY DEAD")
+    
+    if enemy5_alive:
+        dx = enemy5_x - player_x
+        dy = enemy5_y - player_y
+
+        dist = math.sqrt(dx * dx + dy * dy)
+
+        angle_to_enemy5 = math.atan2(dy, dx)
+        angle_diff = angle_to_enemy5 - player_angle
+
+        angle_diff = (angle_diff + math.pi) % (2 * math.pi) - math.pi
+
+        if abs(angle_diff) < 0.1:
+            ray_x = player_x
+            ray_y = player_y
+
+            for i in range(int(dist * 10)):
+                ray_x += math.cos(player_angle) * 0.1
+                ray_y += math.sin(player_angle) * 0.1
+
+                if world_map[int(ray_y)][int(ray_x)] == 1:
+                    return
+            enemy5_hp -= 25
+            print("HIT!", enemy5_hp)
+
+            if enemy5_hp <= 0:
+                enemy5_alive = False
+                print("ENEMY DEAD")
 
 
 def melee_attack():
-    global enemy_hp, enemy_alive,enemy1_hp,enemy1_alive,enemy2_alive,enemy2_hp,enemy3_alive,enemy3_hp,enemy4_alive,enemy4_hp
+    global enemy_hp, enemy_alive,enemy1_hp,enemy1_alive,enemy2_alive,enemy2_hp,enemy3_alive,enemy3_hp,enemy4_alive,enemy4_hp,enemy5_hp, enemy5_alive
 
     if enemy_alive:
         dx = enemy_x - player_x
@@ -546,6 +578,20 @@ def melee_attack():
 
             if enemy4_hp <= 0:
                 enemy4_alive = False
+                print("ENEMY DEAD")
+    
+    if enemy5_alive:
+        dx = enemy5_x - player_x
+        dy = enemy5_y - player_y
+
+        dist = math.sqrt(dx * dx + dy * dy)
+
+        if dist < 1.5:
+            enemy5_hp -= 50
+            print("MELEE HIT", enemy5_hp)
+
+            if enemy5_hp <= 0:
+                enemy5_alive = False
                 print("ENEMY DEAD")
 
 
@@ -672,6 +718,38 @@ def draw_enemy4():
     size = int(min(800 / (dist + 0.1), HEIGHT))
 
     sprite = pygame.transform.scale(enemy4_img, (size, size))
+
+    screen.blit(
+        sprite,
+        (screen_x - size // 2, HEIGHT // 2 - size // 2)
+    )
+
+def draw_enemy5():
+    if not enemy5_alive:
+        return
+    dx = enemy5_x - player_x
+    dy = enemy5_y - player_y
+
+    dist = math.sqrt(dx * dx + dy * dy)
+
+    angle = math.atan2(dy, dx) - player_angle
+
+    angle = (angle + math.pi) % (2 * math.pi) - math.pi
+
+    if abs(angle) > math.pi / 4:
+        return
+
+    screen_x = int((angle / (math.pi / 4)) * (WIDTH / 2) + WIDTH / 2)
+
+    if screen_x < 0 or screen_x >= WIDTH:
+        return
+
+    if dist > z_buffer[screen_x]:
+        return
+
+    size = int(min(800 / (dist + 0.1), HEIGHT))
+
+    sprite = pygame.transform.scale(enemy5_img, (size, size))
 
     screen.blit(
         sprite,
@@ -823,6 +901,7 @@ while running:
         draw_enemy2()
         draw_enemy3()
         draw_enemy4()
+        draw_enemy5()
         draw_flashlight()
         pygame.draw.line(screen, (255, 255, 255), (WIDTH // 2 - 10, HEIGHT // 2), (WIDTH // 2 + 10, HEIGHT // 2), 2)
         pygame.draw.line(screen, (255, 255, 255), (WIDTH // 2, HEIGHT // 2 - 10), (WIDTH // 2, HEIGHT // 2 + 10), 2)
